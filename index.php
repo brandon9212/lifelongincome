bf9dfd5d63d400a89d04b2f98c574264044b6e3a
root <brandon92@hotmail.sg>
2019-10-15 22:30:02 +0800

    add email

--- index.php
+++ index.php
@@ -259,12 +259,12 @@
         <?php
         // the message
         $msg = "First line of text\nSecond line of text";
-        
+
         // use wordwrap() if lines are longer than 70 characters
         $msg = wordwrap($msg,70);
 
         // send email
-        mail("brandon92@hotmail.sg","testing",$msg);
+        mail("samuel.webhosting@gmail.com","testing",$msg);
         ?>
   <!-- Contact form-->
   <section class="call-to-action text-white text-center">
